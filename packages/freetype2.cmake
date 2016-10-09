1962d70a53459d976266458ce8451b5f9dfb67e8
Martin Herkt <lachs0r@srsfckn.biz>
2016-10-09 02:13:16 +0200

    Revert "FT2: checkout working commit"
    This reverts commit cbed4b25b411fac33fe0717cbd80e945789b546e.

--- packages/freetype2.cmake
+++ packages/freetype2.cmake
@@ -3,7 +3,6 @@ configure_file(${CMAKE_CURRENT_SOURCE_DIR}/ft2exec.in ${CMAKE_CURRENT_BINARY_DIR
 ExternalProject_Add(freetype2
     DEPENDS libpng zlib
     GIT_REPOSITORY "git://git.sv.gnu.org/freetype/freetype2.git"
-    GIT_TAG 2e09812c5121413fa29692a46b310983cb3de3e8
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${CMAKE_CURRENT_BINARY_DIR}/ft2exec <SOURCE_DIR>/configure
         --build=${HOST_ARCH}
@@ -18,5 +17,5 @@ ExternalProject_Add(freetype2
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
-# force_rebuild_git(freetype2)
+force_rebuild_git(freetype2)
 autogen(freetype2)
