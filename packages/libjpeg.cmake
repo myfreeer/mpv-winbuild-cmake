41a1a9527619c976788e8f7f04678e06fda3054b
Martin Herkt <lachs0r@srsfckn.biz>
2016-10-09 02:12:30 +0200

    libjpeg-turbo: update to 1.5.1

--- packages/libjpeg.cmake
+++ packages/libjpeg.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(libjpeg
     DEPENDS gcc
-    URL "http://download.sourceforge.net/libjpeg-turbo/libjpeg-turbo-1.4.2.tar.gz"
-    URL_HASH SHA256=521bb5d3043e7ac063ce3026d9a59cc2ab2e9636c655a2515af5f4706122233e
+    URL "http://download.sourceforge.net/libjpeg-turbo/libjpeg-turbo-1.5.1.tar.gz"
+    URL_HASH SHA256=41429d3d253017433f66e3d472b8c7d998491d2f41caa7306b8d9a6f2a2c666c
     CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
